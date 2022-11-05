440e12f492315bb17e0d76243562a08846b0b096
Caio Vinícius <caio.carvalho134@hotmail.com>
2022-11-05 19:11:21 -0300

    Popula dados Produto e Categoria, associação muitos para muitos

--- src/main/java/com/caio/petshop/repository/ProdutoRepository.java
+++ src/main/java/com/caio/petshop/repository/ProdutoRepository.java
@@ -2,9 +2,9 @@ package com.caio.petshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.caio.petshop.domain.Categoria;
+import com.caio.petshop.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 	
 }
