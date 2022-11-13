897456ea6c2e6c7370a11041c467e2f2a4ed7ff2
Caio Vinícius <caio.carvalho134@hotmail.com>
2022-11-13 11:34:49 -0300

    Listando Pessoas com DTO

--- src/main/java/com/caio/petshop/resources/ServicoResource.java
+++ src/main/java/com/caio/petshop/resources/ServicoResource.java
@@ -1,7 +1,6 @@
 package com.caio.petshop.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
