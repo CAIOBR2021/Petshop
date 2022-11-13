a9edf56b329d95e371d9e5c0a6ad6c8933e463bb
Caio Vinícius <caio.carvalho134@hotmail.com>
2022-11-12 21:48:57 -0300

    CRUD de Categoria (POST, PUT, DELETE) + Manipulação do Exception

--- src/main/java/com/caio/petshop/service/CategoriaService.java
+++ src/main/java/com/caio/petshop/service/CategoriaService.java
@@ -3,10 +3,12 @@ package com.caio.petshop.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.caio.petshop.domain.Categoria;
 import com.caio.petshop.repository.CategoriaRepository;
+import com.caio.petshop.service.exceptions.DataIntegrityException;
 import com.caio.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 @Service
@@ -20,5 +22,31 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjetoNaoEncontradoException("Objeto não encontrado. ID: " + id + ", Tipo: " + Categoria.class.getName()));
 		
 	}
+	
+	public Categoria insert(Categoria obj) { 
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) { 
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) { 
+		find(id);
+		
+		try {
+			
+			repo.deleteById(id);
+			
+		} catch (DataIntegrityViolationException e) {
+			
+			throw new DataIntegrityException("Categoria possui produtos, não é possível deletar!"); 
+			
+		}
+		
+	}
+	
 	
 }
