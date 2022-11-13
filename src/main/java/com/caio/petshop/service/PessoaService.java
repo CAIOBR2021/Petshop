e1882565a3374fb1c6fe06ef0e6a8d67c286a1b0
Caio Vinícius <caio.carvalho134@hotmail.com>
2022-11-13 11:10:13 -0300

    POST, PUT e DELETE de Pessoa e Servico + findAll

--- src/main/java/com/caio/petshop/service/PessoaService.java
+++ src/main/java/com/caio/petshop/service/PessoaService.java
@@ -1,12 +1,15 @@
 package com.caio.petshop.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.caio.petshop.domain.Pessoa;
 import com.caio.petshop.repository.PessoaRepository;
+import com.caio.petshop.service.exceptions.DataIntegrityException;
 import com.caio.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 @Service
@@ -21,4 +24,32 @@ public class PessoaService {
 		
 	}
 	
+	public Pessoa insert(Pessoa obj) { 
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Pessoa update(Pessoa obj) { 
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) { 
+		find(id);
+		
+		try {
+			
+			repo.deleteById(id);
+			
+		} catch (DataIntegrityViolationException e) {
+			
+			throw new DataIntegrityException("Pessoa não pode ser deletada!"); 
+			
+		}		
+	}
+
+	public List<Pessoa> findAll() {
+		
+		return repo.findAll();
+	}
 }
